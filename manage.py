9bca1fd5cf73015ef8560848f399a3334abf4bb1
zazy_scetch <melkiy7dc@mail.ru>
2020-10-02 05:28:06 -0700

    lesson_1. Working static site

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'geekshop.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django_shop.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
