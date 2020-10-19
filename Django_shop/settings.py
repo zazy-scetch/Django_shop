7eec5a461541b1e2427528429ea2d7511010aaef
zazy_scetch <melkiy7dc@mail.ru>
2020-10-19 23:12:08 +0300

    Lesson 6. Login fix

--- Django_shop/settings.py
+++ Django_shop/settings.py
@@ -131,3 +131,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 MEDIA_URL = "/media/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+# Set login path:
+#   https://docs.djangoproject.com/en/2.2/ref/settings/#login-url
+LOGIN_URL = "authnapp:login"
