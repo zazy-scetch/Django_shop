1b3f1027bea5766fbbe5b51a2281f479280e2ab6
Zezin Aleksandr <57712823+zazy-scetch@users.noreply.github.com>
2020-10-29 12:18:55 +0300

    Lesson 8 (#9)
    * lesson_8. CRUD products.
    
    * lesson_8. Paginator.
    
    * lesson_8. CBV.
    
    * lesson_8. Template filters. The END.

--- Django_shop/urls.py
+++ Django_shop/urls.py
@@ -8,7 +8,7 @@ import mainapp.views as mainapp
 urlpatterns = [
     path("", mainapp.main, name="main"),
     path("products/", include("mainapp.urls", namespace="products")),
-    path("contacts/", mainapp.contacts, name="contacts"),
+    path("contact/", mainapp.contact, name="contact"),
     path("auth/", include("authnapp.urls", namespace="auth")),
     path("basket/", include("basketapp.urls", namespace="basket")),
     path("admin/", include("adminapp.urls", namespace="admin")),
