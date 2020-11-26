0956bd82d6a4178487598800a6084325514794fe
zazy_scetch <melkiy7dc@mail.ru>
2020-11-26 17:15:17 +0300

    lesson_7. Low level cache

--- Django_shop/settings.py
+++ Django_shop/settings.py
@@ -263,3 +263,19 @@ if DEBUG:
         "debug_toolbar.panels.profiling.ProfilingPanel",
         "template_profiler_panel.panels.template.TemplateProfilerPanel",
     ]
+
+
+CACHE_MIDDLEWARE_ALIAS = "default"
+CACHE_MIDDLEWARE_SECONDS = 120
+CACHE_MIDDLEWARE_KEY_PREFIX = "geekbrains"
+
+# Be carefull if you have Windows! Install Memcached before run project!
+#     https://www.ubergizmo.com/how-to/install-memcached-windows/
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.memcached.MemcachedCache",
+        "LOCATION": "127.0.0.1:11211",
+    }
+}
+
+LOW_CACHE = True
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,6 +1,7 @@
 import random
 
 from django.conf import settings
+from django.core.cache import cache
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
@@ -8,9 +9,84 @@ from django.utils import timezone
 from .models import Contact, Product, ProductCategory
 
 
+def get_links_menu():
+    if settings.LOW_CACHE:
+        key = "links_menu"
+        links_menu = cache.get(key)
+        if links_menu is None:
+            # print(f'caching {key}')
+            links_menu = ProductCategory.objects.filter(is_active=True)
+            cache.set(key, links_menu)
+        return links_menu
+    else:
+        return ProductCategory.objects.filter(is_active=True)
+
+
+def get_category(pk):
+    if settings.LOW_CACHE:
+        key = f"category_{pk}"
+        category = cache.get(key)
+        if category is None:
+            category = get_object_or_404(ProductCategory, pk=pk)
+            cache.set(key, category)
+        return category
+    else:
+        return get_object_or_404(ProductCategory, pk=pk)
+
+
+def get_products():
+    if settings.LOW_CACHE:
+        key = "products"
+        products = cache.get(key)
+        if products is None:
+            products = Product.objects.filter(is_active=True, category__is_active=True).select_related("category")
+            cache.set(key, products)
+        return products
+    else:
+        return Product.objects.filter(is_active=True, category__is_active=True).select_related("category")
+
+
+def get_product(pk):
+    if settings.LOW_CACHE:
+        key = f"product_{pk}"
+        product = cache.get(key)
+        if product is None:
+            product = get_object_or_404(Product, pk=pk)
+            cache.set(key, product)
+        return product
+    else:
+        return get_object_or_404(Product, pk=pk)
+
+
+def get_products_orederd_by_price():
+    if settings.LOW_CACHE:
+        key = "products_orederd_by_price"
+        products = cache.get(key)
+        if products is None:
+            products = Product.objects.filter(is_active=True, category__is_active=True).order_by("price")
+            cache.set(key, products)
+        return products
+    else:
+        return Product.objects.filter(is_active=True, category__is_active=True).order_by("price")
+
+
+def get_products_in_category_orederd_by_price(pk):
+    if settings.LOW_CACHE:
+        key = f"products_in_category_orederd_by_price_{pk}"
+        products = cache.get(key)
+        if products is None:
+            products = Product.objects.filter(category__pk=pk, is_active=True, category__is_active=True).order_by(
+                "price"
+            )
+            cache.set(key, products)
+        return products
+    else:
+        return Product.objects.filter(category__pk=pk, is_active=True, category__is_active=True).order_by("price")
+
+
 def main(request):
     title = "главная"
-    products = Product.objects.filter(is_active=True, category__is_active=True)[:3]
+    products = get_products()[:3]
     content = {"title": title, "products": products, "media_url": settings.MEDIA_URL}
     return render(request, "mainapp/index.html", content)
 
@@ -26,7 +102,7 @@ def main(request):
 
 
 def get_hot_product_list():
-    products = Product.objects.filter(is_active=True, category__is_active=True).select_related("category")
+    products = get_products()
     hot_product = random.sample(list(products), 1)[0]
     hot_list = products.exclude(pk=hot_product.pk)[:3]
     return (hot_product, hot_list)
@@ -34,17 +110,15 @@ def get_hot_product_list():
 
 def products(request, pk=None, page=1):
     title = "продукты"
-    links_menu = ProductCategory.objects.filter(is_active=True)
+    links_menu = get_links_menu()
 
     if pk is not None:
         if str(pk) == str(0):
             category = {"pk": 0, "name": "все"}
-            products = Product.objects.filter(is_active=True, category__is_active=True).order_by("price")
+            products = get_products_orederd_by_price()
         else:
-            category = get_object_or_404(ProductCategory, pk=pk)
-            products = Product.objects.filter(category__pk=pk, is_active=True, category__is_active=True).order_by(
-                "price"
-            )
+            category = get_category(pk)
+            products = get_products_in_category_orederd_by_price(pk)
 
         paginator = Paginator(products, 2)
         try:
@@ -79,8 +153,8 @@ def product(request, pk):
     title = "продукты"
     content = {
         "title": title,
-        "links_menu": ProductCategory.objects.filter(is_active=True),
-        "product": get_object_or_404(Product, pk=pk),
+        "links_menu": get_links_menu(),
+        "product": get_product(pk),
         "media_url": settings.MEDIA_URL,
     }
     return render(request, "mainapp/product.html", content)
